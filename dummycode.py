0df6aa76dc2ff29913c5400ab9169b933da48e38
Lavender Awuor <awuor053@gmail.com>
2026-03-09 19:03:05 +0300

    broken code

---
+++ dummycode.py
@@ -0,0 +1,11 @@
+#This is a comment- it is ignored by Python
+#Variables are created by assigning a value
+name= "World"
+greeting= "Hello"
+
+#The print () function displays output to the console
+print(f"(greeting), (name)!")
+
+#Simple loop example
+for i in range(3):
+    print(f"iteration: {i}")
